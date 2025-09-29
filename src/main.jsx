f638a8f9afebb00f7e173169b10133a38d2fd17f
kathiravan C <124439980+Kathiravan292@users.noreply.github.com>
2025-09-29 19:50:38 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App';
 import {BrowserRouter} from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './assets/Store';
-
+ 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
